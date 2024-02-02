5f99253582a9e5118bf80047d0485eab63d8e8e7
kg15n <kg15n@Ragnar>
2024-02-02 13:09:44 +0530

    RegisterServlet java

--- NITCOF-1/src/main/java/com/user/servlet/RegisterServlet.java
+++ NITCOF-1/src/main/java/com/user/servlet/RegisterServlet.java
@@ -27,6 +27,7 @@ public class RegisterServlet extends HttpServlet {
 			
 //			System.out.println(name+" "+email+" "+phone+" "+password+" "+check); 
 			
+			
 			User us = new  User();
 			us.setName(name);
 			us.setEmail(email);
