d3327e56bc88085a39fb84e768225de4b03bc23a
Karan Sonkar <ksjnva@gmail.com>
2024-02-05 22:44:21 +0530

    add abstract module

--- NITCOF-1/src/main/java/com/cfp/controller/DocumentController.java
+++ NITCOF-1/src/main/java/com/cfp/controller/DocumentController.java
@@ -5,6 +5,7 @@ import com.cfp.repository.DocumentRepository;
 import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServletResponse;
 import org.apache.catalina.filters.ExpiresFilter;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.data.repository.query.Param;
@@ -18,21 +19,22 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 public class DocumentController {
     @Autowired
     private DocumentRepository repo;
-    @GetMapping("/")
-    public String uploadDocument(Model model){
+    @GetMapping("/myUploads")
+    public String uploadDocument(@NotNull Model model){
         List<Document> listDocs=repo.findAll();
         model.addAttribute("listDocs",listDocs);
         return "home";
 
     }
     @PostMapping("/upload")
-    public String uploadFile(@RequestParam("document")MultipartFile multipartFile,RedirectAttributes ra ) throws IOException {
-        String filename= StringUtils.cleanPath(multipartFile.getOriginalFilename());
+    public String uploadFile(@RequestParam("document") @NotNull MultipartFile multipartFile, @NotNull RedirectAttributes ra ) throws IOException {
+        String filename= StringUtils.cleanPath(Objects.requireNonNull(multipartFile.getOriginalFilename()));
         Document document=new Document();
         document.setName(filename);
         document.setContent(multipartFile.getBytes());
