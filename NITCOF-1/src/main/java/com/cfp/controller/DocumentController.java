a9ee6a44087883551fd2461244fb7c4e5eb97dcf
Karan Sonkar <ksjnva@gmail.com>
2024-02-13 11:13:44 +0530

    abstract upload module

--- NITCOF-1/src/main/java/com/cfp/controller/DocumentController.java
+++ NITCOF-1/src/main/java/com/cfp/controller/DocumentController.java
@@ -7,12 +7,10 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -21,7 +19,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
-@RestController
+@Controller
 public class DocumentController {
     @Autowired
     private DocumentRepository repo;
--- NITCOF-1/src/main/java/com/cfp/controller/UserController.java
+++ NITCOF-1/src/main/java/com/cfp/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-@RestController
+@Controller
 public class UserController {
     @Autowired
     private UserRepo repo;
