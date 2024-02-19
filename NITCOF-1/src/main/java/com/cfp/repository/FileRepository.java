b8b5f8c0be6aceea4ed30d51c0b5f0326c2a41c4
Karan Sonkar <ksjnva@gmail.com>
2024-02-19 10:10:23 +0530

    some modification in frontend and add paper module

---
+++ NITCOF-1/src/main/java/com/cfp/repository/FileRepository.java
@@ -0,0 +1,10 @@
+package com.cfp.repository;
+
+
+import com.cfp.entity.FileEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FileRepository extends JpaRepository<FileEntity,Long> {
+}
