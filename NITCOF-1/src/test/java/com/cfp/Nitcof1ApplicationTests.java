d4000297b392a80863d39114f71843980fc07a99
Karan Sonkar <ksjnva@gmail.com>
2024-03-12 13:17:46 +0530

    HomeControllerTest Modification

--- NITCOF-1/src/test/java/com/cfp/Nitcof1ApplicationTests.java
+++ NITCOF-1/src/test/java/com/cfp/Nitcof1ApplicationTests.java
@@ -1,13 +1,20 @@
 package com.cfp;
 
+import com.cfp.controller.HomeController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class Nitcof1ApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testHomePage() {
+		Object result = HomeController.HomePage();
+		assertEquals(ModelAndView.class, result.getClass());
+		assertEquals("index", ((ModelAndView) result).getViewName());
 	}
 
 }
